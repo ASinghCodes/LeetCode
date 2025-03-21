75cf4e791dd80688ef9a192abb32fcbb215cdb1a
Aman Singh <asinghcodes@gmail.com>
2025-03-20 22:11:15 -0500

    eh

---
+++ arrays.py
@@ -0,0 +1,8 @@
+def p1929():
+    return "Way too easy of a problem to be realistically asked. Noticed using array + array was faster than concat(array, array). \nThis is likely due to javascript environment leetcode uses according to chatgpt, but both methods work and this problem will never be asked irl."
+
+def p1769():
+    return "Didn't solve it. I thought I had to return list but people are returning array? Either way lets try this again later."
+
+if __name__ == "__main__":
+    print("No problem selected")
